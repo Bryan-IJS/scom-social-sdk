d8e3bf6b6c2e8516fc8246d64aba31a516f47265
ojochau <ojochau@instajs.com>
2025-04-07 18:18:19 +0800

    Update IAgentSkillInfo to remove unused property

--- src/interfaces/misc.ts
+++ src/interfaces/misc.ts
@@ -237,10 +237,7 @@ export interface IAgentSkillInfo {
 	name: string;
 	chatId?: string;
 	category?: string;
-	knowledge?: string[];
-	capabilities?: IAgentCapability;
-	authentication?: string;
-	schema?: string;
+	data?: any;
 }
 
 export enum AgentTaskScheduleType {
